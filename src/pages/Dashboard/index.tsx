0b47a039b5fd3052014e299f364ca075b858a719
Murilo Maia <murilomaia.bb@gmail.com>
2020-07-11 20:42:35 -0300

    chore/rewriting-balance-code

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -40,8 +40,14 @@ const Dashboard: React.FC = () => {
         balance: balanceResponse,
       } = await (await api.get('transactions')).data;
 
+      const balanceFormatted = {
+        income: formatValue(balanceResponse.income),
+        outcome: formatValue(balanceResponse.outcome),
+        total: formatValue(balanceResponse.total),
+      };
+
       setTransactions(transactionsResponse);
-      setBalance(balanceResponse);
+      setBalance(balanceFormatted);
     }
 
     loadTransactions();
@@ -57,27 +63,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">
-              {`R$ ${formatValue(parseFloat(balance.income))}`}
-            </h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">
-              {`R$ ${formatValue(parseFloat(balance.outcome))}`}
-            </h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">
-              {`R$ ${formatValue(parseFloat(balance.total))}`}
-            </h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
